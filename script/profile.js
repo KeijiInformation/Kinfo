c0d124adc6783e07c554d845be2889f115468327
KeijiInformation <keijiinformation417@gmail.com>
2022-04-23 15:30:28 +0900

    reform: profile/about-programming

--- script/profile.js
+++ script/profile.js
@@ -1,28 +1,14 @@
 // スキルチャートここから
-    const skillChart = document.querySelector('#skill-chart');
+const skillChart = document.querySelector('#skill-chart');
 
     // 引数設定ここから
-    function makeChartOption(direction, title) {
-        axesOption = {
-            display: true,
-            suggestedMax: 5,
-            suggestedMin: 0,
-            callback: function(value, index, values) {
-                return 'レベル' + value;
-            },
-            ticks: {
-                stepSize: 1,
+    function makeChartOption(title) {
+        scales = {
+            r: {
+                min: 0,
+                max: 5,
+                ticks: {stepSize: 1}
             }
-        }
-
-        if (direction == 'row') {
-            axes = 'y';
-            scales = {x: axesOption, y: {barPercentage: 0.5}}
-        } else if (direction == 'column') {
-            axes = 'x';
-            scales = {y: axesOption, x: {barPercentage: 0.5}}
-        } else {
-            return false;
         }
 
         let options = {
@@ -30,8 +16,7 @@
                 display: true,
                 text: title,
             },
-            indexAxis: axes,
-            scales: scales
+            scales: scales,
         }
 
         return options;
@@ -39,149 +24,21 @@
     // 引数設定ここまで
 
     let langChart = new Chart(skillChart, {
-        type: 'bar',
+        type: 'radar',
         data: {
-            labels: [''],
+            labels: ['Python', 'HTML･CSS', 'JavaScript', 'PHP'],
             datasets: [
                 {
-                    label: 'Python',
-                    data: [3],
-                    backgroundColor: "rgba(59,83,255,0.4)",
-                }, {
-                    label: 'HTML･CSS',
-                    data: [2],
-                    backgroundColor: "rgba(255,59,59,0.4)",
-                }, {
-                    label: 'Javascript',
-                    data: [1],
-                    backgroundColor: "rgba(240,255,59,0.4)",
-                }, {
-                    label: 'PHP',
-                    data: [1],
-                    backgroundColor: "rgba(191,59,255,0.4)",
+                    label: '言語',
+                    data: [4, 3, 1, 1],
+                    backgroundColor: 'rgba(97,255,125,0.3)',
+                    borderColor: 'rgba(0,0,0,0.5)',
+                    borderWidth: 1,
+                    pointBackgroundColor: ["rgba(59,83,255,0.4)", "rgba(255,59,59,0.4)", "rgba(240,255,59,0.4)", "rgba(191,59,255,0.4)"],
                 },
             ]
         },
-        options: makeChartOption('column', 'プログラミングスキル')
-    })
-
-    // python
-    const pythonSkillChart = document.querySelector('#Python-skill-chart');
-    let pythonChart = new Chart(pythonSkillChart, {
-        type: 'bar',
-        data: {
-            labels: ['Webアプリ開発', 'GUIツール作成', 'Webスクレイピング'],
-            datasets: [
-                {
-                    label: 'Django',
-                    data: [3, null, null],
-                    skipNull: true,
-                    backgroundColor: 'rgba(2,99,0,0.5)',
-                },
-                {
-                    label: 'Flask',
-                    data: [2, null, null],
-                    skipNull: true,
-                    backgroundColor: 'rgba(130,130,130,0.5)',
-                },
-                {
-                    label: 'Tkinter',
-                    data: [null, 3, null],
-                    skipNull: true,
-                    backgroundColor: 'rgba(59,83,255,0.4)',
-                },
-                {
-                    label: 'Kivy',
-                    data: [null, 3, null],
-                    skipNull: true,
-                    backgroundColor: 'rgba(255,59,59,0.4)',
-                },
-                {
-                    label: 'Selenium',
-                    data: [null, null, 3],
-                    skipNull: true,
-                    backgroundColor: 'rgba(237,113,0,0.5)',
-                },
-                {
-                    label: 'BeautifulSoup',
-                    data: [null, null, 3],
-                    skipNull: true,
-                    backgroundColor: 'rgba(240,255,59,0.4)',
-                }
-            ]
-        },
-        options: makeChartOption('column', 'Pythonスキル詳細')
-    })
-
-    // HTML･CSS
-    const webSkillChart = document.querySelector('#HTML･CSS-skill-chart');
-    let webChart = new Chart(webSkillChart, {
-        type: 'bar',
-        data: {
-            labels: [''],
-            datasets: [
-                {
-                    label: 'コーディング',
-                    data: [4],
-                    backgroundColor: 'rgba(191,59,255,0.4)',
-                },
-                {
-                    label: 'デザイン',
-                    data: [2],
-                    backgroundColor: 'rgba(240,255,59,0.4)',
-                },
-                {
-                    label: 'レスポンシブ対応',
-                    data: [3],
-                    backgroundColor: 'rgba(255,59,59,0.4)',
-                },
-            ]
-        },
-        options: makeChartOption('column', 'HTML･CSSスキル詳細')
-    })
-
-    // JavaScript
-    const jsSkillChart = document.querySelector('#JavaScript-skill-chart');
-    let jsChart = new Chart(jsSkillChart, {
-        type: 'bar',
-        data: {
-            labels: [''],
-            datasets: [
-                {
-                    label: 'Node.js',
-                    data: [0],
-                    backgroundColor: 'rgba(191,59,255,0.4)',
-                },
-                {
-                    label: 'アニメーション',
-                    data: [2],
-                    backgroundColor: 'rgba(240,255,59,0.4)',
-                },
-            ]
-        },
-        options: makeChartOption('column', 'JavaScriptスキル詳細')
-    })
-
-    // PHP
-    const phpSkillChart = document.querySelector('#PHP-skill-chart');
-    let phpChart = new Chart(phpSkillChart, {
-        type: 'bar',
-        data: {
-            labels: [''],
-            datasets: [
-                {
-                    label: 'バックエンド処理',
-                    data: [1],
-                    backgroundColor: 'rgba(191,59,255,0.4)',
-                },
-                {
-                    label: 'WordPressテーマ作成',
-                    data: [2],
-                    backgroundColor: 'rgba(240,255,59,0.4)',
-                },
-            ]
-        },
-        options: makeChartOption('column', 'JavaScriptスキル詳細')
+        options: makeChartOption('各言語の修得状況'),
     })
 // スキルチャートここまで
 
@@ -192,29 +49,66 @@
 
 // 言語ボタンここから
     const langBtnList = document.querySelectorAll('.icon-area li');
+    const detailFooterDesignList = document.querySelectorAll('.detail-footer-design');
+
+    // メイン処理
     function langBtnClick(target) {
         let detailTarget;
         let activeClassName;
+
         // active -> none (targetの切り替えのみでよい)
         if (target.classList.contains('active')) {
             target.classList.remove('active');
+
+            // 説明枠の取得とオープンここから
             activeClassName = target.className.split(' ')[0];
             detailTarget = document.querySelector('.language-detail-content' + '.' + activeClassName);
             detailTarget.classList.remove('active');
+            // 説明枠の取得とオープンここまで
+
+            // 説明枠フッターのアニメーションここから
+            detailFooterDesign = document.querySelectorAll('.language-detail-content' + '.' + activeClassName + ' .detail-footer-design');
+            for (let item of detailFooterDesign) {
+                item.classList.remove('active');
+            }
+            // 説明枠フッターのアニメーションここまで
+
         // none -> active (targetの切り替えと他要素のremoveが必要)
         } else {
+
             for (langBtn of langBtnList) {
                 if (langBtn.classList.contains('active')) {
                     langBtn.classList.remove('active');
+
+                    // 説明枠の取得とクローズここから
                     activeClassName = langBtn.className.split(' ')[0];
                     detailTarget = document.querySelector('.language-detail-content' + '.' + activeClassName);
                     detailTarget.classList.remove('active');
+                    // 説明枠の取得とクローズここまで
+
+                    // 説明枠フッターのアニメーションここから
+                    detailFooterDesignTargets = document.querySelectorAll('.language-detail-content' + '.' + activeClassName + ' .detail-footer-design');
+                    for (let item of detailFooterDesignTargets) {
+                        item.classList.remove('active');
+                    }
+                    // 説明枠フッターのアニメーションここまで
+
                 }
             }
+            // 説明枠の取得とオープンここから
             target.classList.add('active');
             activeClassName = target.className.split(' ')[0];
             detailTarget = document.querySelector('.language-detail-content' + '.' + activeClassName);
             detailTarget.classList.add('active');
+            // 説明枠の取得とオープンここまで
+
+            // 説明枠フッターのアニメーションここから
+            detailFooterDesignTargets = document.querySelectorAll('.language-detail-content' + '.' + activeClassName + ' .detail-footer-design');
+            for (let item of detailFooterDesignTargets) {
+                item.classList.add('active');
+            }
+            // 説明枠フッターのアニメーションここまで
+
         }
     }
 
@@ -224,3 +118,8 @@
         }, false)
     }
 // 言語ボタンここまで
+
+
+
+
+
