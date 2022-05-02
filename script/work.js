0f32f1abedf023809a4bff66c3d1bb52cad7c80a
KeijiInformation <keijiinformation417@gmail.com>
2022-05-02 22:17:02 +0900

    complete: pc-design

--- script/work.js
+++ script/work.js
@@ -1,20 +1,35 @@
+// ポップアップ時にスクロール固定する処理
+function fixBackground(event) {
+    // event.preventDefault();
+}
+// ポップアップ時にスクロール固定する処理
+
+
+
+
+
+
 // ポップアップボタントリガーここから
     const viewMoreBtnList = document.querySelectorAll(".button-area");
 
     function viewMorePopupOpen(target) {
         let bgCoverBox = document.querySelector(".bg-cover-box");
         let workMoreArea = document.querySelector(".work-more-area");
+        let closeBtn = document.querySelector(".close-button");
 
         let targetClassName = target.className.split(" ")[1];
         let targetDOM = document.querySelector(".work-more-contents." + targetClassName);
         bgCoverBox.classList.add("open");
         workMoreArea.classList.add("open");
         targetDOM.classList.add("open");
+        closeBtn.classList.add("open");
     }
 
     for (let viewMoreBtn of viewMoreBtnList) {
         viewMoreBtn.addEventListener("click", function() {
             viewMorePopupOpen(viewMoreBtn);
+            document.addEventListener("touchmove", fixBackground, {passive: false});
+            document.addEventListener("mousewheel", fixBackground, {passive: false});
         }, false);
     }
 // ポップアップボタントリガーここから
@@ -31,12 +46,22 @@
     function popupClose() {
         let bgCoverBox = document.querySelector(".bg-cover-box");
         let workMoreArea = document.querySelector(".work-more-area");
-        let targetDOM = document.querySelector(".work-more-contents.open");
+        let closeBtn = document.querySelector(".close-button");
+        let targetDOM = document.querySelector(".work-more-contents.open")
         bgCoverBox.classList.remove("open");
         workMoreArea.classList.remove("open");
         targetDOM.classList.remove("open");
+        closeBtn.classList.remove("open");
     }
 
-    popupCloseBtn.addEventListener("click", popupClose, false);
-    bgCoverBox.addEventListener("click", popupClose, false);
+    popupCloseBtn.addEventListener("click", function() {
+        popupClose();
+        document.removeEventListener("touchmove", fixBackground, {passive: false});
+        document.removeEventListener("mousewheel", fixBackground, {passive: false});
+    }, false);
+    bgCoverBox.addEventListener("click", function() {
+        popupClose();
+        document.removeEventListener("touchmove", fixBackground, {passive: false});
+        document.removeEventListener("mousewheel", fixBackground, {passive: false});
+    }, false);
 // ポップアップクローズここまで
